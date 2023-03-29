2cb39978c649ffc9e2dfe9305f1bad7da8fb7a02
Mahir Absar <mahir.mohsin@northsouth.edu>
2023-03-29 11:38:37 +0600

    complete design for register/login page

---
+++ client/src/components/Alert.js
@@ -0,0 +1,4 @@
+const Alert = () => {
+  return <div className='alert alert-success'>Alert goes here</div>
+}
+export default Alert
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Logo } from './Logo'
+export { default as FormRow } from './FormRow'
+export { default as Alert } from './Alert'
