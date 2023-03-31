7233655bf22065d79fa27fe73274f162e94f6c6b
Mahir Absar <mahir.mohsin@northsouth.edu>
2023-03-31 22:44:03 +0600

    add protected route feat

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import Landing from './pages/Landing'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Dashboard, Register, Error } from './pages'
+import { Register, Error, ProtectedRoute } from './pages'
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
-export { default as Dashboard } from './Dashboard'
-
 export { default as Register } from './Register'
 
 export { default as Error } from './Error'
+
+export { default as ProtectedRoute } from './ProtectedRoute'
