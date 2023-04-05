40704faa3f8a3b765c70e0b8ee1e1052e9c8145c
Mahir Absar <mahir.mohsin@northsouth.edu>
2023-04-05 13:28:20 +0600

    setup sidebar for moble and big screen

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,6 +1,34 @@
+import { FaTimes } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { useAppContext } from '../context/appContext'
+import { NavLink } from 'react-router-dom'
+import Logo from './Logo'
+import links from '../utils/links'
+import NavLinks from './NavLinks'
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>
+  const { showSidebar, toggleSidebar } = useAppContext()
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            <NavLinks toggleSidebar={toggleSidebar}></NavLinks>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 export default SmallSidebar
